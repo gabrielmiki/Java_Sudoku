aaa4ecbef933431001df0a6e6bd92d198178e834
Gabriel de Almeida Miki <96574190+gabrielmiki@users.noreply.github.com>
2023-06-04 18:45:50 +0000

    Update code

--- JavaDescktopSudokuDemo/src/sudoku/problemDomain/Coordinates.java
+++ JavaDescktopSudokuDemo/src/sudoku/problemDomain/Coordinates.java
@@ -1,24 +1,32 @@
 package sudoku.problemDomain;
 
 public class Coordinates {
+
     private final int x;
     private final int y;
 
     public Coordinates(int x, int y) {
+
         this.x = x;
         this.y = y;
+
     }
 
     public int getX() {
+
         return x;
+
     }
 
     public int getY() {
+
         return y;
+
     }
 
     @Override
     public boolean equals(Object o) {
+
         if (this == o) return true;
 
         if (o == null || getClass() != o.getClass()) return false;
@@ -26,10 +34,13 @@ public class Coordinates {
         Coordinates that = (Coordinates) o;
 
         return x == that.x && y == that.y;
+
     }
 
     @Override
     public int hashCode() {
+
         return Objects.hash(x, y);
+        
     }
 }
--- JavaDescktopSudokuDemo/src/sudoku/problemDomain/SudokuGame.java
+++ JavaDescktopSudokuDemo/src/sudoku/problemDomain/SudokuGame.java
@@ -5,21 +5,28 @@ import sudoku.constants.GameState;
 import java.io.Serializable;
 
 public class SudokuGame implements Serializable{
+
     private final GameState gameState;
     private final int[][] gridState;
 
     public static final int GRID_BOUNDARY = 9;
 
     public SudokuGame(GameState gameState, int[][] gridState) {
+
         this.gameState = gameState;
         this.gridState = gridState;
+
     }
 
     public GameState getGameState() {
+
         return gameState;
+
     }
 
     public int[][] getCopyOfGridState() {
+
         return SudokuUtilities.copyToNewArray(gridState);
+        
     }
 }
